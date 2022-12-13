316f3fb8f2031476b9dcbf519f2512485f5eac32
Rushane WIlson <rushwilson1@gmail.com>
2022-12-13 08:06:35 -0500

    Finished Course

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import userRouter from './routes/user.js';
+import tourRouter from './routes/tour.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/users', userRouter);
+app.use('/tour', tourRouter);
+app.get('/', (req, res) => {
+  res.send('Welcome to tour API');
+});
+
+const port = process.env.SERVER_PORT || 5000;
+const databaseUrl = process.env.CONNECTION_STRING;
+
+mongoose
+  .connect(databaseUrl)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
